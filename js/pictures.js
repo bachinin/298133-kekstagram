d0a221991d632422b4e55b9dc16236c4aa99eaeb
Artem <bachinin@gmail.com>
2018-02-24 11:32:44 +0500

    Выполнил задание 10 Личный проект kekstagram: милый DOM

---
+++ js/pictures.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var photos = [];
+var commentsList = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+// Генерируем массив с фотографиями
+for (var i = 1; i <= 25; i++) {
+  var photo = {
+    url: 'photos/' + i + '.jpg',
+    likes: getRandomIntInclusive(15, 200),
+    comments: [].push(commentsList[getRandomIntInclusive(0, commentsList.length - 1)])
+  };
+  photos.push(photo);
+}
+
+var pictureTemplate = document.querySelector('#picture-template').content;
+
+var renderPicture = function (photo) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture').getElementsByTagName('img')[0].src = photo.url;
+  pictureElement.querySelector('.picture-likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture-comments').textContent = photo.comments;
+
+  return pictureElement;
+}
+
+var fragment = document.createDocumentFragment();
+for (var j = 0; j < photos.length; j++) {
+  fragment.appendChild(renderPicture(photos[j]));
+}
+
+var pictureContainer = document.querySelector('.pictures');
+pictureContainer.appendChild(fragment);
+
+var galleryOverlay = document.querySelector('.gallery-overlay');
+galleryOverlay.classList.remove('hidden');
+
+galleryOverlay.querySelector('.gallery-overlay-image').src = photos[0].url;
+galleryOverlay.querySelector('.likes-count').textContent =  photos[0].likes;
+galleryOverlay.querySelector('.comments-count').textContent = photos[0].comments;
+
